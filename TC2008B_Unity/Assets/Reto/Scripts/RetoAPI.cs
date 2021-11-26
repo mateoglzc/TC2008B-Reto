9834af55f68b8e16cee3936c6dc5912cc5f49d65
Mateo Gonzalez Cosio <mateoglzc@hotmail.com>
2021-11-26 14:18:52 -0600

    To fight the unbeatable foe

---
+++ TC2008B_Unity/Assets/Reto/Scripts/RetoAPI.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class RetoAPI : MonoBehaviour
+{
+    [SerializeField] GameObject donkeys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
